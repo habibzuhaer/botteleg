06c5d1279867adc4cb1c178f5d44c08a8b127411
habibzuhaer <39887123+habibzuhaer@users.noreply.github.com>
2025-08-14 17:56:56 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,49 @@
+# config.py
+TRADING_GROUPS = {
+    "Основные пары (15m)": {
+        "timeframe": "15m",
+        "symbols": {
+            'ADA': 'ADA/USDT',
+            'APT': 'APT/USDT',
+            'AVAX': 'AVAX/USDT',
+            'ARB': 'ARB/USDT',
+            'BNB': 'BNB/USDT',
+            'DOGE': 'DOGE/USDT',
+            'ETH': 'ETH/USDT',
+            'ENA': 'ENA/USDT',
+            'FIL': 'FIL/USDT',
+            'GRT': 'GRT/USDT',
+            'HBAR': 'HBAR/USDT',
+            'INJ': 'INJ/USDT',
+            'JUP': 'JUP/USDT',
+            'LTC': 'LTC/USDT',
+            'LINK': 'LINK/USDT',
+            'MANA': 'MANA/USDT',
+            'NEAR': 'NEAR/USDT',
+            'SEI': 'SEI/USDT',
+            'SOL': 'SOL/USDT',
+            'SUI': 'SUI/USDT',
+            'TIA': 'TIA/USDT',
+            'TON': 'TON/USDT',
+            'XLM': 'XLM/USDT',
+            'XRP': 'XRP/USDT'
+        }
+    },
+    "Крупные капиталы (1h)": {
+        "timeframe": "1h",
+        "symbols": {
+            'BTC': 'BTC/USDT',
+            'ETH': 'ETH/USDT',
+            'BNB': 'BNB/USDT',
+            'SOL': 'SOL/USDT'
+        }
+    },
+    "Быстрые пары (5m)": {
+        "timeframe": "5m",
+        "symbols": {
+            'DOGE': 'DOGE/USDT',
+            'SHIB': 'SHIB/USDT',
+            'PEPE': 'PEPE/USDT'
+        }
+    }
+}
